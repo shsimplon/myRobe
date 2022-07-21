2dff03e3f7304149a924f626fa2f849bf8ae165f
samira <houacine.samira@gmail.com>
2022-07-21 20:07:03 +0200

    reservation

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user ( id int PRIMARY KEY, name varchar(30)  NOT NULL,
+CREATE TABLE user ( id varchar(36) PRIMARY KEY, username varchar(30)  NOT NULL,
 address varchar(255) NOT NULL,
 email
 varchar(20) UNIQUE NOT NULL,
@@ -11,11 +11,11 @@ varchar(10) NOT NULL );
 
 
 CREATE TABLE categorie 
-( id int KEY, name_categorie varchar(30) NOT NULL ) ;
+( id varchar(36) PRIMARY KEY, name_categorie varchar(30) NOT NULL ) ;
 
 
 CREATE TABLE dress (
-id int PRIMARY KEY , name varchar(30) NOT NULL, 
+id varchar(36) PRIMARY KEY , name varchar(30) NOT NULL, 
 description
 varchar(255) UNIQUE NOT NULL,
 size
@@ -24,18 +24,18 @@ image
 varchar(50) NOT NULL,
 price
 varchar(10) NOT NULL ,
- categorieId int,
+ categorieId varchar(36)  ,
 CONSTRAINT fk_categorieId FOREIGN KEY (categorieId) REFERENCES categorie (id)
 );
 
 
-CREATE TABLE `dress_reservation` (
-id int(36) NOT NULL,
+CREATE TABLE `reservation` (
+id varchar(36) PRIMARY KEY,
  date_departure varchar(30) NOT NULL, 
 
 reurn_date varchar(15) NOT NULL,
-  dressId int(36) NOT NULL,
-  reservationId int(36) NOT NULL,
+  dressId varchar(36)  NOT NULL,
+  reservationId varchar(36)   NOT NULL,
     
   CONSTRAINT `FK_dressId` FOREIGN KEY (`dressId`) REFERENCES `dress` (`id`),
   CONSTRAINT `FK_reservationId` FOREIGN KEY (`reservationId`) REFERENCES `reservation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
